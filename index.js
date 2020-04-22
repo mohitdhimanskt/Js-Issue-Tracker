d560677a79e7bc8147878f88173140e9e64b67ec
Mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-04-22 21:21:12 +0530

    Fourcommit

---
+++ index.js
@@ -0,0 +1,18 @@
+document.getElementById('issueInputForm').addEventListener('sumbit',saveIssue);
+
+function saveIssue(e){
+    var issueDesc = document.getElementById('issueDescInput').value;
+    var issueSeverity = document.getElementById('issueSeverityInput').value;
+    var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+    var issueId = change.guid();
+    var issueStatus = 'OPen';
+
+    var issue ={
+        id: issueId,
+        description: issueDesc,
+        severity: issueSeverity,
+        assignedTo: issueAssignedTo,
+        status: issueStatus
+    }
+
+}
