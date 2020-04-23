232cad87d15ab529f3597e3099186beb8eb01c65
Mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-04-23 11:34:46 +0530

    Sevencommit

--- index.js
+++ index.js
@@ -36,5 +36,15 @@ function setStatusClosed(id){
         }
     }
     localStorage.setItem('issue',JSON.stringify(issues));
-    fetchIssue();
+    fetchIssue();   
 }
+function  deleteIssue(id){
+    var issues = JSON.parse(localStorage.getItem('issues'));
+     for(var i = 0; i < issues.length; i++){
+         if(issues[i].id == id){
+             issues.splice(i,1);
+         }
+     }
+     localStorage.setItem('issues',JSON.stringify(issues));
+     fetchIssues();
+    }
